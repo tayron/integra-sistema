cabe6964c88e9497004a4c6a99af14efb7b10859
Tayron Miranda <contato@tayron.com.br>
2020-07-15 17:54:43 -0300

    Ajuste na mensagem de exibição de execução do sistema

--- main.go
+++ main.go
@@ -13,19 +13,21 @@ import (
 
 func init() {
 	godotenv.Load()
-	fmt.Println(os.Getenv("NOME_SISTEMA"))
 	models.CriarTabelaIntegracao()
 }
 
 func main() {
 	http.HandleFunc("/", handlers.InicioHandler)
 
+	fmt.Printf("####################### %s - versão %s #######################\n",
+		os.Getenv("NOME_SISTEMA"), os.Getenv("VERSAO_SISTEMA"))
+
 	fmt.Printf("Servidor executando em: http://127.0.0.1:%s\n", os.Getenv("PORTA_SERVIDOR"))
-	enderecoServidor := fmt.Sprintf(":%s", os.Getenv("PORTA_SERVIDOR"))
 
 	if os.Getenv("PORTA_SERVIDOR") == "" {
 		panic("Deve-se informar a porta de execução do servidor no arquivo .env: PORTA_SERVIDOR=3003")
 	}
 
+	enderecoServidor := fmt.Sprintf(":%s", os.Getenv("PORTA_SERVIDOR"))
 	log.Fatalln(http.ListenAndServe(enderecoServidor, nil))
 }
