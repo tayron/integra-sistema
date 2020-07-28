99f0f0967449c79709d5f9bba96fe827228483ec
Tayron Miranda <contato@tayron.com.br>
2020-07-28 17:51:44 -0300

    Refatorado as rotas

--- routes/rotas.go
+++ routes/rotas.go
@@ -17,14 +17,11 @@ func CarregarRotas() {
 	r.HandleFunc("/", controllers.IndexApplication).Methods("GET")
 
 	r.HandleFunc("/integracoes", controllers.ListarIntegracao).Methods("GET")
-	r.HandleFunc("/integracao/cadastrar", controllers.CadastrarIntegracao).Methods("GET")
-	r.HandleFunc("/integracao/cadastrar", controllers.CadastrarIntegracao).Methods("POST")
-	r.HandleFunc("/integracao/editar/{id:[0-9]+}", controllers.EditarIntegracao).Methods("GET")
-	r.HandleFunc("/integracao/editar/{id:[0-9]+}", controllers.EditarIntegracao).Methods("POST")
+	r.HandleFunc("/integracao/cadastrar", controllers.CadastrarIntegracao)
+	r.HandleFunc("/integracao/editar/{id:[0-9]+}", controllers.EditarIntegracao)
 	r.HandleFunc("/integracao/excluir", controllers.ExcluirIntegracao).Methods("POST")
 
-	r.HandleFunc("/parametros/integracao/{id:[0-9]+}", controllers.ListarParametro).Methods("GET")
-	r.HandleFunc("/parametros/integracao/{id:[0-9]+}", controllers.ListarParametro).Methods("POST")
+	r.HandleFunc("/parametros/integracao/{id:[0-9]+}", controllers.ListarParametro)
 	r.HandleFunc("/parametro/excluir", controllers.ExcluirParametro).Methods("POST")
 
 	r.HandleFunc("/logs/integracao/{id:[0-9]+}", controllers.ListarLog).Methods("GET")
